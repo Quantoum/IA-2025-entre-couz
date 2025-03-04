dd16679dc7e1e1e67104c5c94c95e2222ffed459
arnikatoutcourt <arnaud.ullens@student.uclouvain.be>
2025-03-04 16:27:18 +0100

    finished tapestry

--- P1/tapestry/tapestry.py
+++ P1/tapestry/tapestry.py
@@ -1,39 +1,43 @@
 from pycsp3 import *
 
-
-def solve_tapestry(clues: list[list[(int, int)]]) -> list[list[(int, int)]]:
-    # Put your code here
-    SIZE = len(clues)
-
-    shape = VarArray(size=[SIZE,SIZE], dom=range(1, SIZE+1))
-    color = VarArray(size=[SIZE,SIZE], dom=range(1, SIZE+1))
-
-    # x = clues ?
-    satisfy(
-        # constraint 1 (rows)
-            # shape
-            [AllDifferent(shape[i]) for i in range(SIZE)],
-            # color
-            [AllDifferent(color[i]) for i in range(SIZE)],
-
-        # constraint 2 (cols)
-            # shape
-            [AllDifferent(shape[:, i]) for i in range(SIZE)],
-            # color
-            [AllDifferent(color[:, i]) for i in range(SIZE)],
-
-        # constraint 3 (combinaison)
-            [AllDifferent()]
-
-    )
-
+def solve_tapestry(clues):
+    n = len(clues)
+    
+    # Shapes and colors. format inupt : (s, c)
+    shapes = VarArray(size=[n, n], dom=range(1, n+1))
+    colors = VarArray(size=[n, n], dom=range(1, n+1))
+    
+    # constraints
+    for i in range(n):
+        # rows with s and c
+        satisfy(AllDifferent(shapes[i]))
+        satisfy(AllDifferent(colors[i]))
+        # cols with s and c
+        satisfy(AllDifferent(shapes[:, i]))
+        satisfy(AllDifferent(colors[:, i]))
+    
+    # fill with clues
+    for i in range(n):
+        for j in range(n):
+            c_shape, c_color = clues[i][j]
+            if c_shape == 0 or c_color == 0: # ignore the case (empty clue)
+                continue
+            # fill the values
+            satisfy(shapes[i][j] == c_shape)
+            satisfy(colors[i][j] == c_color)
+    
+    # encode bcz VarArray cannot f*ck tuples  (linear encoding)
+    encoded_pairs = [shapes[i][j] * (n + 1) + colors[i][j] for i in range(n) for j in range(n)]
+    satisfy(AllDifferent(encoded_pairs)) # each pair !=
+    
+    # solve model
     if solve(solver=CHOCO) is SAT:
-        print("SATISFIABLE")
-        print(values(pair))
+        # reconstruct the tuples
+        solution = [[(shapes[i][j].value, colors[i][j].value) for j in range(n)] for i in range(n)]
+        return solution
     else:
-        print("UNSATISFIABLE")
+        return None
 
-    return None
 
 def verify_format(solution: list[list[(int, int)]], n: int):
     validity = True
@@ -57,7 +61,7 @@ def parse_instance(input_file: str) -> list[list[(int, int)]]:
     with open(input_file) as input:
         lines = input.readlines()
     n = int(lines[0].strip())
-    clues = [[(0, 0) for i in range(n)] for j in range(n)]
+    clues = [[(0, 0) for _ in range(n)] for _ in range(n)]
     for line in lines[1:]:
         i, j, s, c = line.strip().split(" ")
         clues[int(i)][int(j)] = (int(s), int(c))
