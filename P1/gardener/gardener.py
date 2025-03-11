03f1c054ab8b2f05b99177a024beeef419b064a0
Quintus <q.depierpont@student.uclouvain.be>
2025-03-11 14:30:31 +0100

    gardener copy of restricted added

--- P1/gardener/gardener.py
+++ P1/gardener/gardener.py
@@ -2,9 +2,33 @@ from pycsp3 import *
 
 
 def solve_gardener(instructions: list[list[int]]) -> list[list[int]]:
-    # Put your code here
+    if n == 0:
+        return []
+    heights = VarArray(size=n, dom=range(1, n + 1))
+    visible = VarArray(size=n, dom={0, 1})
+
+    # Ensure all heights are unique
+    satisfy(AllDifferent(heights))
     
-    return None
+    # The first hedge is always visible
+    satisfy(visible[0] == 1)
+
+    for i in range(1, n):
+        conditions = [heights[i] > heights[j] for j in range(i)]
+
+        # Combine conditions with logical AND
+        cond = conditions[0]
+        for c in conditions[1:]: # verify that cond is greater than every other
+            cond = cond & c # if c is false (other one greater than him), then cond is false (and it never changes)
+        satisfy(visible[i] == cond) # if the tower is visible
+
+    # Total visible must match the instruction
+    satisfy(Sum(visible) == instruction)
+
+    if solve(solver=CHOCO) is SAT:
+        return [heights[i].value for i in range(n)]
+    else:
+        return None
 
 def verify_format(solution: list[list[int]], n: int):
     validity = True
